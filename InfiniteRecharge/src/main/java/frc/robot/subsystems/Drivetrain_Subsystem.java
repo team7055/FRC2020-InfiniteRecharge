e3dbeb93028cc830939c7b9bde1d967fd5ddb82a
Jimmy <jimmystry@gmail.com>
2020-01-09 19:56:33 -0500

    Added initialization of motors and drive

--- InfiniteRecharge/src/main/java/frc/robot/subsystems/Drivetrain_Subsystem.java
+++ InfiniteRecharge/src/main/java/frc/robot/subsystems/Drivetrain_Subsystem.java
@@ -7,14 +7,30 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Drivetrain_Subsystem extends SubsystemBase {
   /**
    * Creates a new Drivetrain_Subsystem.
    */
+  private Talon frontRight;
+  private Talon frontLeft;
+  private Talon rearRight;
+  private Talon rearLeft;
+  private MecanumDrive drive;
+  
   public Drivetrain_Subsystem() {
+    // Initialize motors with ports from Constants.java
+    frontRight = new Talon(Constants.MOTOR_FRONT_RIGHT);
+    frontLeft = new Talon(Constants.MOTOR_FRONT_LEFT);
+    rearRight = new Talon(Constants.MOTOR_REAR_RIGHT);
+    rearLeft = new Talon(Constants.MOTOR_REAR_LEFT);
 
+    // Initialize drive with motors
+    drive = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
   }
 
   @Override
